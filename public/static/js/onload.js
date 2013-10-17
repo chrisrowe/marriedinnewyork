10fde7c3e55a293443a4b3716a6502140e2bfe92
Chris Rowe <mail@chrisrowe.net>
2013-10-17 22:51:13 +0100

    more progress

--- public/static/js/onload.js
+++ public/static/js/onload.js
@@ -4,12 +4,12 @@ $(function() {
         $(this).toggleClass("full");
     });
 
-    $(".nav_pri li").on("click", function(){
-        $(".nav_pri li").removeClass("cur");
-        $(this).addClass("cur");
-        var section = $(this).attr("data-sectionlink");
-        $(".section").addClass("hidden");
-        $("." + section).removeClass("hidden");
-    })
+    // $(".nav_pri li").on("click", function(){
+    //     $(".nav_pri li").removeClass("cur");
+    //     $(this).addClass("cur");
+    //     var section = $(this).attr("data-sectionlink");
+    //     $(".section").addClass("hidden");
+    //     $("." + section).removeClass("hidden");
+    // })
 
 });
