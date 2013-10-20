feaee5070b03c591a4bbd46d7139ffe2f8d98976
Chris Rowe <mail@chrisrowe.net>
2013-10-20 16:30:58 +0100

    nearly there

--- public/static/js/onload.js
+++ public/static/js/onload.js
@@ -1,15 +1,27 @@
 $(function() {
 
+    // TOGGLE IMAGE SIZES
     $("figure").on("click", function(){
         $(this).toggleClass("full");
     });
 
-    // $(".nav_pri li").on("click", function(){
-    //     $(".nav_pri li").removeClass("cur");
-    //     $(this).addClass("cur");
-    //     var section = $(this).attr("data-sectionlink");
-    //     $(".section").addClass("hidden");
-    //     $("." + section).removeClass("hidden");
-    // })
+    // ADJUST TEXTAREA SIZE IF NEEDED
+    $('textarea').autosize();
+
+    // LOCAL LINKS CHANGE PAGE
+    $("a[href^='#']").on("click", function(e){
+        e.preventDefault();
+        var tab = $(this).attr("href").replace("#","");
+        $(".nav_pri li").removeClass("cur");
+        $(".nav_pri a[href='#"+tab+"']").parents("li").addClass("cur");
+        $(".section").addClass("hidden");
+        $(".section." + tab).removeClass("hidden");
+        $('html,body').animate({
+            scrollTop: $(".content_pri").offset().top - 46
+        }, 10);
+        return false;
+    });
+
+
 
 });
